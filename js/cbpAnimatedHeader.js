18e69ce8147de90558945f5301a4f21a48916e0c
sptq <sptq@hslbn.pl>
2015-03-21 09:00:39 +0100

    Add workshops, newsletter, fix branches, fix ui

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -25,14 +25,14 @@ var cbpAnimatedHeader = (function() {
 	}
 
 	function scrollPage() {
-		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
-		}
-		didScroll = false;
+    // var sy = scrollY();
+    // if ( sy >= changeHeaderOn ) {
+    //  classie.add( header, 'navbar-shrink' );
+    // }
+    // else {
+    //  classie.remove( header, 'navbar-shrink' );
+    // }
+    // didScroll = false;
 	}
 
 	function scrollY() {
